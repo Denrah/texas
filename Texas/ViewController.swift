34b8dce041cd7f72b9bcbda9b0bf8b140a7fd94f
Denis Sharapov <denisharapov@yandex.ru>
2021-10-19 22:19:00 +0700

    Initial Commit

---
+++ Texas/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Texas
+//
+//  Created by National Team on 19.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
